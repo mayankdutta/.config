a5aac719b3418e71593cb38b3b384287b08e3203
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-06-28 18:38:40 +0530

    done shifting line in visual mode

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -45,8 +45,11 @@ vim.api.nvim_set_keymap('n', '<C-n>', ':bnext<CR>', {noremap = true, silent = tr
 vim.api.nvim_set_keymap('n', '<C-p>', ':bprevious<CR>', {noremap = true, silent = true})
 
 -- Move selected line / block of text in visual mode
-vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
+
+-- vnoremap J :m '>+1<CR>gv=gv
+-- vnoremap K :m '<-2<CR>gv=gv
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv=gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv=gv', {noremap = true, silent = true})
 
 
 -- Better nav for omnicomplete
