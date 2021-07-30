762e0b7bd1ffca98295617d892598751d41d4c20
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2021-07-30 12:49:26 +0530

    Update lv-config.lua

--- lvim/lv-config.lua
+++ lvim/lv-config.lua
@@ -1,25 +1,14 @@
---[[
-O is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
 -- general
+lvim.format_on_save = true
+lvim.lint_on_save = true
+lvim.colorscheme = "spacegray"
 
-O.format_on_save = true
-O.lint_on_save = true
-O.completion.autocomplete = true
-O.colorscheme = "spacegray"
-O.default_options.wrap = true
-O.default_options.timeoutlen = 100
 -- keymappings
-O.keys.leader_key = "space"
+lvim.leader = "space"
 -- overwrite the key-mappings provided by LunarVim for any mode, or leave it empty to keep them
--- O.keys.normal_mode = {
+-- lvim.keys.normal_mode = {
 --   Page down/up
 --   {'[d', '<PageUp>'},
 --   {']d', '<PageDown>'},
@@ -29,7 +18,7 @@ O.keys.leader_key = "space"
 --   {'<S-Tab>', ':bprevious<CR>'},
 -- }
 -- if you just want to augment the existing ones then use the utility function
--- require("lv-utils").add_keymap_insert_mode({ silent = true }, {
+-- require("utils").add_keymap_insert_mode({ silent = true }, {
 -- { "<C-s>", ":w<cr>" },
 -- { "<C-c>", "<ESC>" },
 -- })
@@ -38,22 +27,20 @@ O.keys.leader_key = "space"
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-O.plugin.dashboard.active = true
-O.plugin.terminal.active = true
-O.plugin.zen.active = false
-O.plugin.zen.window.height = 0.90
-O.plugin.nvimtree.side = "left"
-O.plugin.nvimtree.show_icons.git = 0
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
-O.treesitter.ensure_installed = "maintained"
-O.treesitter.ignore_install = { "haskell" }
-O.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
 
 -- generic LSP settings
 -- you can set a custom on_attach function that will be used for all the language servers
 -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- O.lsp.on_attach_callback = function(client, bufnr)
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
 --   local function buf_set_option(...)
 --     vim.api.nvim_buf_set_option(bufnr, ...)
 --   end
@@ -61,55 +48,8 @@ O.treesitter.highlight.enabled = true
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 
--- python
-O.lang.python.diagnostics.virtual_text = true
-O.lang.python.analysis.use_library_code_types = true
--- To change default formatter from yapf to black
--- O.lang.python.formatter.exe = "black"
--- O.lang.python.formatter.args = {"-"}
--- To change enabled linters
--- https://github.com/mfussenegger/nvim-lint#available-linters
--- O.lang.python.linters = { "flake8", "pylint", "mypy", ... }
-
--- go
--- To change default formatter from gofmt to goimports
--- O.lang.formatter.go.exe = "goimports"
-
--- javascript
-O.lang.tsserver.linter = nil
-
--- rust
--- O.lang.rust.rust_tools = true
--- O.lang.rust.formatter = {
---   exe = "rustfmt",
---   args = {"--emit=stdout", "--edition=2018"},
--- }
-
--- scala
--- O.lang.scala.metals.active = true
--- O.lang.scala.metals.server_version = "0.10.5",
-
---LaTeX
--- Options: https://github.com/latex-lsp/texlab/blob/master/docs/options.md
-O.lang.latex.active = true
-O.lang.latex.aux_directory = "."
-O.lang.latex.bibtex_formatter = "texlab"
-O.lang.latex.build.args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" }
-O.lang.latex.build.executable = "latexmk"
-O.lang.latex.build.forward_search_after = false
-O.lang.latex.build.on_save = false
-O.lang.latex.chktex.on_edit = false
-O.lang.latex.chktex.on_open_and_save = false
-O.lang.latex.diagnostics_delay = 300
-O.lang.latex.formatter_line_length = 80
-O.lang.latex.forward_search.executable = "zathura"
-O.lang.latex.latex_formatter = "latexindent"
-O.lang.latex.latexindent.modify_line_breaks = false
--- O.lang.latex.auto_save = false
--- O.lang.latex.ignore_errors = { }
-
 -- Additional Plugins
--- O.user_plugins = {
+-- lvim.plugins = {
 --     {"folke/tokyonight.nvim"}, {
 --         "ray-x/lsp_signature.nvim",
 --         config = function() require"lsp_signature".on_attach() end,
@@ -118,13 +58,82 @@ O.lang.latex.latexindent.modify_line_breaks = false
 -- }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- O.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+-- }
 
 -- Additional Leader bindings for WhichKey
+
+
+
+-- mine
+
+lvim.lang.tailwindcss.active = true
+lvim.lang.emmet.active = true
+
+-- Whichkey
 -- O.user_which_key = {
---   A = {
---     name = "+Custom Leader Keys",
---     a = { "<cmd>echo 'first custom command'<cr>", "Description for a" },
---     b = { "<cmd>echo 'second custom command'<cr>", "Description for b" },
---   },
+      -- t = { '<cmd>split term://fish | resize 28 <CR>', "split terminal"},
+      -- v = { '<cmd>vsplit<CR>', 'vertical split'},
+      -- h = { '<cmd>split<CR>', 'horizontal split'},
+      -- y = { '"+y', 'copy selected line'},
+      -- Y = { 'gg"+yG', 'copy whole file'},
+      -- TODO add these
+      -- p = {
+      --   S = { "<cmd>PackerStatus<cr>", "Status" },
+      --   C = { "<cmd>PackerClean<cr>", "Clean" },
+      -- }
 -- }
+
+lvim.builtin.which_key.mappings.t = { "<cmd>split term://fish | resize 28 <cr>", "Terminal" }
+lvim.builtin.which_key.mappings.v = { "<cmd>vsplit<cr>", "vertical split" }
+lvim.builtin.which_key.mappings.h = { "<cmd>split<cr>", "horizontal split" }
+lvim.builtin.which_key.mappings.Y = { 'gg"+yG', "Copy whole file" }
+lvim.builtin.which_key.mappings.y = { '"+y', "Copy Selected line" }
+
+lvim.builtin.treesitter.playground.enable = true
+lvim.builtin.treesitter.matchup.enable = true
+lvim.builtin.treesitter.autotag.enable = true
+lvim.builtin.treesitter.indent.enable = false
+
+vim.cmd [[
+  au BufEnter * if &buftype == 'terminal' | :startinsert | endif
+  set autoindent
+  set cindent
+]]
+
+lvim.plugins= {
+  {
+    "unblevable/quick-scope",
+    config = function()
+      vim.cmd [[
+      let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
+      ]]
+    end,
+  },
+  {
+    "andymass/vim-matchup",
+    event = "CursorMoved",
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    event = "BufRead",
+  },
+{
+    "nvim-treesitter/playground",
+    event = "BufRead",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+  },
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+}
+
+
