391528ec6861614312db212a84bf9257507db45a
mayank dutta <smayankdutt@gmail.com>
2021-10-04 21:35:06 +0530

    lvim updated

--- lvim.bak/config.lua
+++ lvim.bak/config.lua
@@ -7,6 +7,8 @@ lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
