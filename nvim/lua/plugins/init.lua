a696d1ce845842c511d5b928239d0d3698df9b5a
mayank dutta <smayankdutt@gmail.com>
2024-06-08 13:23:57 +0530

    neochad tree modified

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -40,4 +40,17 @@ return {
   },
 
   -- nvim-tree
+
+  -- file managing , picker etc
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = function()
+      return require "configs.nvimtree"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "nvimtree")
+      require("nvim-tree").setup(opts)
+    end,
+  },
 }
