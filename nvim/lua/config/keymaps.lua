f356e16b71061afd021be4c4a105e6bacccaec9f
mayank dutta <smayankdutt@gmail.com>
2024-06-09 12:54:39 +0530

    lazyvim updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,14 +19,16 @@
 
 local map = vim.keymap.set
 
--- Resize window using <ctrl> arrow keys
 map("n", "<Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
--- vim.keymap.set(
---   "n",
+map({ "v", "n" }, "<leader>y", '"+y', { noremap = true, desc = "copying a line" })
+map("n", "<leader>Y", "gg\"+yG''", { desc = "Copy whole file" })
+
+-- map(
+--   { "n", "i", "v" },
 --   "<C-d>",
 --   require("telescope.actions").delete_buffer,
 --   { noremap = true, silent = true, desc = "Deleting Buffer" }
