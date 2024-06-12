d9eb4a52a5d9411e3a67ba50db8f830c926dc1a4
mayank dutta <smayankdutt@gmail.com>
2024-06-12 20:39:18 +0530

    several updates

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,43 +1,108 @@
-reload("user.lunarvim")
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
 
--- reload("user.keymaps.defaults")
-reload("user.keymaps.keymap")
+local map = vim.keymap.set
+
+map("i", "jk", "<ESC>")
+
+-- map("n", "<S-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+-- map("n", "<S-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+
+map("v", "<leader>y", '"+y', { noremap = true })
+map("n", "<leader>y", '"+y', { noremap = true })
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "J", "mzJ`z") -- keep the cursor at the beginning while joining the line.
+map("x", "<leader>p", [["_dP]])
+
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+map("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "vertical split" })
+map("n", "<leader>h", "<cmd>split<cr>", { desc = "horizontal split" })
+map("n", "<leader>n", "<cmd>noh<cr>", { desc = "No highlighting" })
+map("n", "<leader>Y", "gg\"+yG''", { desc = "Copy whole file" })
+map("n", "<leader>u", "<cmd>UndotreeToggle<cr>", { desc = "undo tree" })
+-- map("n", "<leader>f", "<cmd>MaximizerToggle<cr>", {desc = "full focus "} )
+map("n", "<leader>q", "<cmd>q<cr>", { desc = "full focus " })
+
+map("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", { desc = "Code Action" })
+map("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", { desc = "Code Action" })
+map("n", "<leader>ld", "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", { desc = "Buffer Diagnostics" })
+map("n", "<leader>lw", "<cmd>Telescope diagnostics<cr>", { desc = "Diagnostics" })
+map("n", "<leader>lf", "<cmd>lua require('lvim.lsp.utils').format()<cr>", { desc = "Format" })
+map("n", "<leader>li", "<cmd>LspInfo<cr>", { desc = "Info" })
+map("n", "<leader>lI", "<cmd>Mason<cr>", { desc = "Mason Info" })
+map("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next()<cr>", { desc = "Next Diagnostic" })
+map("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev()<cr>", { desc = "Prev Diagnostic" })
+map("n", "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", { desc = "CodeLens Action" })
+map("n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<cr>", { desc = "Quickfix" })
+map("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "Rename" })
+map("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Document Symbols" })
+map("n", "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", { desc = "Workspace Symbols" })
+map("n", "<leader>le", "<cmd>Telescope quickfix<cr>", { desc = "Telescope Quickfix" })
 
 
-reload("user.plugins")
---
-reload("user.bufferline")
-reload("user.ignore_pattern")
--- reload("user.telescope")
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-local emmet_options = {
-	capabilities = capabilities,
-	filetypes = {
-		"css",
-		"eruby",
-		"html",
-		"javascript",
-		"javascriptreact",
-		"less",
-		"sass",
-		"scss",
-		"svelte",
-		"pug",
-		"typescriptreact",
-		"vue",
-		"typescript",
-		"xml",
-	},
-
-	root_dir = function()
-		return vim.loop.cwd()
-	end,
-}
-require("lvim.lsp.manager").setup("emmet_ls", emmet_options)
+-- nvim tree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
 
 
+-- new terminals
+
+-- map({ "n", "t" }, "<C-t>", function()
+--   require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+-- end, { desc = "terminal new horizontal term" })
+
+map("n", "<S-h>", "<ESC>^", { desc = "move beginning of line" })
+map("n", "<S-l>", "$", { desc = "move end of line" })
+
+
+lvim.keys.normal_mode["<leader>s"] = nil
+
+-- telescope
+map("n", "<leader>ss", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
+map("n", "<leader>sb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("n", "<leader>sh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
+-- map("n", "<leader>fa", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
+map("n", "<leader>so", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
+map("n", "<leader>sz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+
+map("n", "<leader>gm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+
+map("n", "<leader>st", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
+map("n", "<leader>sh", "<cmd>Telescope themes<CR>", { desc = "telescope nvchad themes" })
+map("n", "<leader>sf", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+map(
+  "n",
+  "<leader>sa",
+  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+  { desc = "telescope find all files" }
+)
+
+
+-- close buffer
+map("n", "<leader>c", function()
+  require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
+
+
+-- Terminal window navigation
+map('x', "<C-h>", "<C-\\><C-N><C-w>h");
+map('x', "<C-j>", "<C-\\><C-N><C-w>j");
+map('x', "<C-k>", "<C-\\><C-N><C-w>k");
+map('x', "<C-l>", "<C-\\><C-N><C-w>l");
 
 
 
+
+-- Resize with arrows
+map('n', '<C-Up>', ":resize -2<CR>");
+map('n', '<C-Down>', ":resize +2<CR>");
+map('n', '<C-Left>', ":vertical resize -2<CR>");
+map('n', '<C-Right>', ":vertical resize +2<CR>");
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
+    -- { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
