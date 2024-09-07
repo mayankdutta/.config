3cd22449505ea11aec924665b0389e2613cd8db1
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2024-09-07 15:18:43 +0530

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,28 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+require('telescope').setup({
+  pickers = {
+      buffers = {
+        show_all_buffers = true, 
+        sort_mru = true, 
+        mappings = {
+          i = {
+            ["<c-d>"] = "delete_buffer", 
+          }, 
+        }, 
+      },
+    },
+  })
+
+vim.api.nvim_create_autocmd({"TermOpen", "BufEnter" }, {
+    pattern = { "*" }, 
+    callback = function()
+      if vim.opt.buftype:get() == "terminal" then 
+        vim.cmd(":startinsert")
+      end
+    })
+
 vim.opt.winbar = "%=%m %f"
 vim.opt.clipboard = ""
+vim.b.autoformat = false
