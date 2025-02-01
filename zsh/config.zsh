648f503a1c7c6e8161a04d84948a11d709f5a2d8
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2025-02-01 17:51:24 +0530

    Update config.zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,40 +1,54 @@
-ulimit -n 1024
 
-alias luamake=/Users/info/.config/lua-language-server/3rd/luamake/luamake
-export PATH=/bin:/usr/bin:/usr/local/bin:/Users/info/bin:/Users/info/.local/bin:/usr/local/bin:/opt/X11/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/info/.cache/zsh4humans/v4/fzf/bin:/usr/local/opt/fzf/bin
-# export STARSHIP_CONFIG=~/.config/starship.toml
-export PATH="$HOME/.cargo/bin:$PATH"
-# export OPENAI_API_KEY=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
+[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh  # This loads NVM
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-path=(~/bin $path)
-alias run='~/bin/run.sh'
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
-chruby ruby-3.0.2
-
-# Example install plugins
-plug "zap-zsh/supercharge"
-plug "zsh-users/zsh-autosuggestions"
-plug "zap-zsh/exa"
-
-myArray=(
-    "$HOME/dir1", 
-    "$HOME/dir2"
+myDirectories=(
+"$HOME/lumi/axp-lumi-login"
+"$HOME/lumi/axp-lumi-home"
+"$HOME/lumi/axp-lumi-data-market-place/"
+"$HOME/lumi/axp-lumi-use-case-management/"
+"$HOME/lumi/axp-lumi-business-managed-data/"
+"$HOME/lumi/axp-lumi-data-ingestion-management/"
+"$HOME/lumi/axp-lumi-data-transformation-management/"
+"$HOME/lumi/axp-lumi-root/"
+"$HOME/lumi"
+"$HOME/.config/"
+"$HOME/.config/nvim/"
+"$HOME/.config/kitty/"
+"$HOME/.config/tmux/"
+"$HOME/.config/fish"
+"$HOME/.config/alacritty/"
+"$HOME/"
+"$HOME/projects/"
 )
 
 function fzf_jump_to_nvim() {
-    selected_dirs=$(printf '%s\n' "${myArray[@]}" | fzf)
-    if [[ -n "$selected_dirs" ]]; then
-        cd "$seelcted_dirs" || return 1
-    else 
-        echo "No Directory selected"
-    fi
+  selected_firs=$(printf '%s\n' "${myDirectories[@]}" | fzf)
+
+  if [[ -n "$selected_firs" ]]; then
+    cd "$selected_firs" || return 1
+  else echo "No Directory selected."
+  fi
 }
 
 bindkey -s '^f' 'fzf_jump_to_nvim\n'
 
-# plug "wintermi/zsh-starship"
-plug "zsh-users/zsh-syntax-highlighting"
-plug "zsh-users/zsh-history-substring-search"
+alias dks='docker stop $(docker ps -a -q)'
+alias dkr='docker rm $(docker ps -a -q)'
+alias nbm="/Users/mdutt14/.nvm/versions/node/v16.20.2/bin/npm run build"
+alias ntm="/Users/mdutt14/.nvm/versions/node/v16.20.2/bin/npm run test:unit -- -u"
+alias nrm="/Users/mdutt14/.nvm/versions/node/v16.20.2/bin/npm run start"
+alias one="dks && dkr && /Users/mdutt14/.nvm/versions/node/v16.20.2/bin/npm run start"
 
-plug "zap-zsh/completions"
+# alias one="cd /Users/mdutt14/lumi/one-app && npm start -- --root-module-name=axp-lumi-root"
+# alias serve="cd /Users/mdutt14/lumi/ && zsh automate.sh"
+
+# exa stuff
+alias ls='eza -lh --group-directories-first --icons --hyperlink'
+alias lsa='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
+
+/Users/mdutt14/.nvm/versions/node/v16.20.2/bin/npm config set registry https://artifactory.aexp.com/api/npm/npm-virtual
+eval "$(starship init zsh)"
+
